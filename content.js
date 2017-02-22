19b286f39b1c4b0e43f1f4f26732755be66ba157
George Seeger <georgeseeger@hotmail.com>
2017-02-22 15:41:48 +0000

    content.js now destroys the message of desired usernames

--- content.js
+++ content.js
@@ -1,11 +1,20 @@
 // default incase thelist doesn't load
-theList = [
+var theList = [
   "official_hitomitanaka",
   "anri_okita"
 ];
 
 window.onload = function(){
   document.addEventListener('scroll', function(){
-    console.log(theList);
+    console.log(theList)
+    for(var i=0; i < theList.length; i++){
+      var name = theList[i];
+      console.log('//h1/a[@title="'+ name + '"]');
+      var element = document.evaluate( '//h1/a[@title="'+ name + '"]' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
+      console.log(element);
+      if (element && element.parentElement.childNodes[1].innerHTML !== '') {
+        element.parentElement.childNodes[1].innerHTML = '';
+      }
+    }
   });
 };
