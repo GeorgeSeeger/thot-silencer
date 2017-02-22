e8d42f7fe46609a272eecab5cd27cfd30e165f21
George Seeger <georgeseeger@hotmail.com>
2017-02-22 15:20:13 +0000

    Hardcoded list into content.js

--- background.js
+++
@@ -1,4 +0,0 @@
-// default incase thelist doesn't load
-var theList = [
-  "official_hitomitanaka"
-];
--- content.js
+++ content.js
@@ -1,5 +1,11 @@
+// default incase thelist doesn't load
+theList = [
+  "official_hitomitanaka",
+  "anri_okita"
+];
+
 window.onload = function(){
   document.addEventListener('scroll', function(){
-    console.log('you scrolled');
+    console.log(theList);
   });
 };
