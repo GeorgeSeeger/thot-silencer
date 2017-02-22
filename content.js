d263e45234b53c4b2ffbac1c7be1e58bb54f89b9
George Seeger <georgeseeger@hotmail.com>
2017-02-22 20:08:29 +0000

    removed print statements from content.js

--- content.js
+++ content.js
@@ -6,12 +6,9 @@ var theList = [
 
 window.onload = function(){
   document.addEventListener('scroll', function(){
-    console.log(theList)
     for(var i=0; i < theList.length; i++){
       var name = theList[i];
-      console.log('//h1/a[@title="'+ name + '"]');
       var element = document.evaluate( '//h1/a[@title="'+ name + '"]' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue
-      console.log(element);
       if (element && element.parentElement.childNodes[1].innerHTML !== '') {
         element.parentElement.childNodes[1].innerHTML = '';
       }
