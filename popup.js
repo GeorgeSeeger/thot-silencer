084f8da5fe135c4ab14bad6a335c9391d17f7a59
George Seeger <georgeseeger@hotmail.com>
2017-02-22 20:08:54 +0000

    can save new names from popup menu in chrome storage

---
+++ popup.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', function() {
+    document.getElementById('addusername').addEventListener('submit', addUserName);
+});
+
+function addUserName() {
+  event.preventDefault();
+  chrome.storage.sync.get('theList', function(list){
+    var username = encodeURIComponent(document.getElementById('username').value);
+    var theNewList = list['theList']
+    theNewList.push(username);
+    newList(theNewList);
+  });
+}
+
+function printList() {
+  chrome.storage.sync.get('theList', function(list){
+    console.log(list);
+  });
+}
+
+function newList(array) {
+  chrome.storage.sync.set({'theList': array}, function(){
+    console.log('Saved ' + array[array.length - 1]);
+  });
+}
+
+function emptyList() {
+  chrome.storage.sync.remove('theList', function(){
+    console.log('Emptied the list');
+  });
+}
